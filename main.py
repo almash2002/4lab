205d983154520b74d797f86d00960397715d05f1
Almash <yerzhanova2002@gmail.com>
2022-10-04 22:05:30 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,57 @@
+#1
+'''name = 'Almash'
+surname = ' Yerzhanova'
+
+print(name+surname)'''
+
+
+#2
+'''import string
+
+print(string.ascii_letters)
+print(string.digits)
+print(string.punctuation)'''
+
+
+#3
+'''import string
+print(ord('A'),ord('l'),ord('m'),ord('a'),ord('s'),ord('h'))
+print(chr(65),chr(108),chr(109),chr(97),chr(115),chr(104))'''
+
+
+#4
+'''name="Yerzhanova Almash"
+print(len(name))'''
+
+
+#5
+'''s = 'Almash'
+print(s[::-1])'''
+
+
+#6
+'''print('almash'.upper())
+print('yerzhanova almash yerzhanovna'.title())
+print('Almash'.lower())'''
+
+
+#7
+'''print('Student'.endswith('nt'))
+print('Student'.startswith('St'))'''
+
+
+#8
+'''print('almash'.isalpha())
+print('almash-4'.isalpha())'''
+
+
+#9
+'''print('almash'.islower())
+print('ALMASH'.isupper())
+print('2002'.isdigit())
+print('2002'.isnumeric())'''
+
+#10
+print('   Hi!    '.lstrip())
+print('   Hi!    '.rstrip())
+print('   Hi!    '.strip())
